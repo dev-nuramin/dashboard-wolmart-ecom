f2af86a24e7f7341d3d8022eb643893f26366cd2
dev-nuramin <stack.developer51@gmail.com>
2024-01-26 19:01:10 +0600

    update shop page and fix

--- shop/src/router/publicRouter.jsx
+++ shop/src/router/publicRouter.jsx
@@ -1,3 +1,6 @@
+import Blog from "../Pages/Blog/Blog";
+import Cart from "../Pages/Cart/Cart";
+import Checkout from "../Pages/Checkout/Checkout";
 import Home from "../Pages/Home/Home";
 import Shop from "../Pages/Shop/Shop";
 import SinglePage from "../Pages/SinglaPage/SinglePage";
@@ -16,6 +19,21 @@ const publicRoute = [
     {
         path: "/shop/:single",
         element: <SinglePage />
+    }
+    ,
+    {
+        path: "/cart",
+        element: <Cart/>
+    }
+    ,
+    {
+        path: "/blog",
+        element: <Blog />
+    }
+    ,
+    {
+        path: "/checkout",
+        element: <Checkout />
     }
 ];
 
--- shop/src/router/routers.jsx
+++ shop/src/router/routers.jsx
@@ -1,9 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import privateRoute from "./privateRouter";
 import publicRoute from "./publicRouter";
+import Layouts from "../Layouts/Layouts";
 
 // create browser router
-const router = createBrowserRouter([...publicRoute, ...privateRoute])
+const router = createBrowserRouter([
+  {
+    element: <Layouts />,
+    children: [...publicRoute, ...privateRoute],
+  },
+]);
 
 // export browser router
 export default router;
